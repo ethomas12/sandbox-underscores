5e826f5bbd83c114b20460dd4a8f45aeae217a7d
Elaine Thomas <enajera2@cnm.edu>
2016-05-03 10:36:26 -0600

    first commit

---
+++ wp-content/themes/tinfoilcat/footer.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Tin_Foil_Cat
+ */
+
+?>
+
+	</div><!-- #content -->
+
+	<footer id="colophon" class="site-footer" role="contentinfo">
+		<div class="site-info">
+			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'tinfoilcat' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'tinfoilcat' ), 'WordPress' ); ?></a>
+			<span class="sep"> | </span>
+			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'tinfoilcat' ), 'tinfoilcat', '<a href="http://el41net@el41net.com" rel="designer">Elaine Thomas</a>' ); ?>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
