6a7dff769af3ed90c7884dc39e07924e67c57ed5
Elaine Thomas <enajera2@cnm.edu>
2016-05-04 13:35:17 -0600

    fixed toggle menu transition

--- wp-content/themes/tinfoilcat/header.php
+++ wp-content/themes/tinfoilcat/header.php
@@ -65,10 +65,10 @@
 			endif; ?>
 		</div><!-- .site-branding -->
 
-		<nav id="site-navigation" class="main-navigation" role="navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Menu', 'tinfoilcat' ); ?></button>
-			<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu', 'menu_class' => 'primary-menu' ) ); ?>
-		</nav><!-- #site-navigation -->
-	</header><!-- #masthead -->
+				<nav id="site-navigation" class="main-navigation" role="navigation">
+					<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Menu', 'tinfoilcat' ); ?></button>
+					<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu', 'menu_class' => 'nav-menu' ) ); ?>
+				</nav><!-- #site-navigation -->
+			</header><!-- #masthead -->
 
-	<div id="content" class="site-content">
+			<div id="content" class="site-content">
