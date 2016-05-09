d7e22bd71f145ddc065087ca241d6d310a5d06ba
Elaine Thomas <enajera2@cnm.edu>
2016-05-08 22:51:09 -0600

    working on post format

--- wp-content/themes/tinfoilcat/index.php
+++ wp-content/themes/tinfoilcat/index.php
@@ -14,41 +14,92 @@
 
  get_header(); ?>
 
- 	<div id="primary" class="content-area">
- 		<main id="main" class="site-main" role="main">
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
 
- 		<?php if ( have_posts() ) : ?>
+		<?php if ( have_posts() ) : ?>
 
- 			<?php if ( is_home() && ! is_front_page() ) : ?>
- 				<header>
- 					<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
- 				</header>
- 			<?php endif; ?>
+			<?php if ( is_home() && ! is_front_page() ) : ?>
+				<header>
+					<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
+				</header>
+			<?php endif; ?>
 
- 			<?php /* Start the Loop */ ?>
- 			<?php while ( have_posts() ) : the_post(); ?>
+			<?php /* Start the Loop */ ?>
+			<?php while ( have_posts() ) : the_post(); ?>
 
- 				<?php
- 					/*
- 					 * Include the Post-Format-specific template for the content.
- 					 * If you want to override this in a child theme, then include a file
- 					 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
- 					 */
- 					get_template_part( 'template-parts/content', get_post_format() );
- 				?>
+				<?php
+					// Is this the first post of the front page?
+					$first_post = $wp_query->current_post == 0 && !is_paged() && is_front_page();
+					/*
+					 * Include the Post-Format-specific template for the content.
+					 * If you want to override this in a child theme, then include a file
+					 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+					 */
+					if ( $first_post == true ) {
+						get_template_part( 'template-parts/content', 'single' );
+					} else {
+						get_template_part( 'template-parts/content', get_post_format() );
+					}
+				?>
 
- 			<?php endwhile; ?>
+			<?php endwhile; ?>
 
- 			<?php tinfoilcat_paging_nav(); ?>
+			<?php tinfoilcat_paging_nav(); ?>
 
- 		<?php else : ?>
+		<?php else : ?>
 
- 			<?php get_template_part( 'template-parts/content', 'none' ); ?>
+			<?php get_template_part( 'template-parts/content', 'none' ); ?>
 
- 		<?php endif; ?>
+		<?php endif; ?>
 
- 		</main><!-- #main -->
- 	</div><!-- #primary -->
+		</main><!-- #main -->
+	</div><!-- #primary -->
 
- <?php get_sidebar(); ?>
- <?php get_footer(); ?>
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+
+		<?php if ( have_posts() ) : ?>
+
+			<?php if ( is_home() && ! is_front_page() ) : ?>
+				<header>
+					<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
+				</header>
+			<?php endif; ?>
+
+			<?php /* Start the Loop */ ?>
+			<?php while ( have_posts() ) : the_post(); ?>
+
+				<?php
+					// Is this the first post of the front page?
+					$first_post = $wp_query->current_post == 0 && !is_paged() && is_front_page();
+					/*
+					 * Include the Post-Format-specific template for the content.
+					 * If you want to override this in a child theme, then include a file
+					 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+					 */
+					if ( $first_post == true ) {
+						get_template_part( 'template-parts/content', 'single' );
+					} else {
+						get_template_part( 'template-parts/content', get_post_format() );
+					}
+				?>
+
+			<?php endwhile; ?>
+
+			<?php tinfoilcat_paging_nav(); ?>
+
+		<?php else : ?>
+
+			<?php get_template_part( 'template-parts/content', 'none' ); ?>
+
+		<?php endif; ?>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
--- wp-content/themes/tinfoilcat/template-parts/content-single.php
+++ wp-content/themes/tinfoilcat/template-parts/content-single.php
@@ -9,44 +9,59 @@
 
 ?>
 
+<?php global $first_post; ?>
+
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
 
 		<?php
-		if (has_post_thumbnail() ) {?>
-		<figure class="featured-image">
-		<?php the_post_thumbnail(); ?>  <!-- change the default thumbnail sizes here -->
-
-		</figure>
+		if ( has_post_thumbnail() ) { ?>
+			<figure class="featured-image">
+				<?php if ( $first_post == true ) { ?>
+					<a href="<?php echo esc_url( get_permalink() ); ?>" rel="bookmark">
+						<?php the_post_thumbnail(); ?>
+					</a>
+				<?php } else {
+					the_post_thumbnail();
+				}
+				?>
+			</figure>
 		<?php }
 		?>
 
-		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+		<?php
+			if ( $first_post == true ) {
+				the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' );
+			} else {
+				the_title( '<h1 class="entry-title">', '</h1>' );
+			}
+
+		?>
 
 		<?php
-		if ( has_excerpt ($post->ID) ) {
+		if ( has_excerpt( $post->ID ) ) {
 			echo '<div class="deck">';
 			echo '<p>' . get_the_excerpt() . '</p>';
 			echo '</div><!-- .deck -->';
 		}
 		?>
 
-		<div class="entry-meta"> <!-- posted on, date, posted by -->
+		<div class="entry-meta">
 			<?php tinfoilcat_posted_on(); ?>
 		</div><!-- .entry-meta -->
 	</header><!-- .entry-header -->
 
-	<div class="entry-content">   <!--displays the content-->
+	<div class="entry-content">
 		<?php the_content(); ?>
 		<?php
 			wp_link_pages( array(
 				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'tinfoilcat' ),
 				'after'  => '</div>',
 			) );
-		?>  <!-- links to additional pages if the page is paginated -->
+		?>
 	</div><!-- .entry-content -->
 
 	<footer class="entry-footer">
-		<?php tinfoilcat_entry_footer(); ?> <!--shows meta data posted in and list categories tags and edit link -->
+		<?php tinfoilcat_entry_footer(); ?>
 	</footer><!-- .entry-footer -->
 </article><!-- #post-## -->
