ae8b6278a021883931e75294b75cdd0aa2212ca9
Elaine Thomas <enajera2@cnm.edu>
2016-05-13 12:02:08 -0600

    css styles for widgets

--- wp-content/themes/tinfoilcat/functions.php
+++ wp-content/themes/tinfoilcat/functions.php
@@ -100,7 +100,7 @@ add_action( 'after_setup_theme', 'tinfoilcat_content_width', 0 );
  */
 function tinfoilcat_widgets_init() {
 	register_sidebar( array(
-		'name'          => esc_html__( 'Sidebar', 'tinfoilcat' ),
+		'name'          => esc_html__( 'Widget Area', 'tinfoilcat' ),
 		'id'            => 'sidebar-1',
 		'description'   => esc_html__( 'Add widgets here.', 'tinfoilcat' ),
 		'before_widget' => '<section id="%1$s" class="widget %2$s">',
